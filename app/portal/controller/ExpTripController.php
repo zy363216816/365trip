ab133a90f3333a06c5425ed2e5512a235e3b5147
Sunny <363216816@qq.com>
2018-07-18 16:53:36 +0800

    v1.0

--- app/portal/controller/ExpTripController.php
+++ app/portal/controller/ExpTripController.php
@@ -11,7 +11,7 @@ namespace app\portal\controller;
 
 use cmf\controller\HomeBaseController;
 
-class ExpTrripController extends HomeBaseController
+class ExpTripController extends HomeBaseController
 {
 
 }
