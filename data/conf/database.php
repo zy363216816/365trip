50987cbfd33f7681eb150f2b5239500d5fe79cd9
Sunny <363216816@qq.com>
2018-07-18 16:21:05 +0800

    Add files via upload

---
+++ data/conf/database.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * 配置文件
+ */
+
+return [
+    // 数据库类型
+    'type'     => 'mysql',
+    // 服务器地址
+    'hostname' => '127.0.0.1',
+    // 数据库名
+    'database' => '365trip',
+    // 用户名
+    'username' => 'root',
+    // 密码
+    'password' => 'sunny112',
+    // 端口
+    'hostport' => '3306',
+    // 数据库编码默认采用utf8
+    'charset'  => 'utf8mb4',
+    // 数据库表前缀
+    'prefix'   => 'trip_',
+    "authcode" => 'xYIXxOLvvVne8Sm1Xp',
+    //#COOKIE_PREFIX#
+];
