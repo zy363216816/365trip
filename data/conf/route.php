680d860043223d6517437d4f958c326aab153002
Sunny <363216816@qq.com>
2018-07-18 09:25:38 +0800

    v1.0

---
+++ data/conf/route.php
@@ -0,0 +1,36 @@
+<?php	return array (
+  'FestiveVideo/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=4',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  'FestiveVideo' => 
+  array (
+    0 => 'portal/List/index?id=4',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
+  'contacts' => 
+  array (
+    0 => 'portal/Page/index?id=1',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
+);
