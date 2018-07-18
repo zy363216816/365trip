5af3d1bd20515d6b27241be71726ee13721987ad
Sunny <363216816@qq.com>
2018-07-18 16:45:34 +0800

    v1.0

---
+++ app/portal/controller/ExpTrripController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 2018-7-18
+ * Time: 16:43
+ */
+
+namespace app\portal\controller;
+
+
+use cmf\controller\HomeBaseController;
+
+class ExpTrripController extends HomeBaseController
+{
+
+}
